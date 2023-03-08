eca2b4fdec8669f61c5247b87ffa7a9b659e861a
chrisdavidpham <50644282+chrisdavidpham@users.noreply.github.com>
2023-03-08 10:16:29 -0600

    09 fix filename

--- 09/arrayExtensions.ts
+++ 09/arrayExtensions.ts
@@ -1,6 +1,5 @@
 export {}
 
 Array.prototype.unique = function<T>(this: T[], comparator: (a: T, b: T) => boolean): T[] {
-    var test = this.filter((a, i, arr) => arr.findIndex(b => comparator(a,b)) == i);
-    return test;
+    return this.filter((a, i, arr) => arr.findIndex(b => comparator(a,b)) == i);
 }
